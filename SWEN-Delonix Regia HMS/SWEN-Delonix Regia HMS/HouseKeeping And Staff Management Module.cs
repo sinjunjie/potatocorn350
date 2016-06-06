635e06267ae57aebe27029206921294d83bec032
riduwan2016 <mdriduwan@hotmail.com>
2016-06-06 17:54:31 +0800

    wan commit

---
+++ SWEN-Delonix Regia HMS/SWEN-Delonix Regia HMS/HouseKeeping And Staff Management Module.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SWEN_Delonix_Regia_HMS
+{
+    public partial class HouseKeeping_And_Staff_Management_Module : Form
+    {
+        public HouseKeeping_And_Staff_Management_Module()
+        {
+            InitializeComponent();
+        }
+    }
+}
