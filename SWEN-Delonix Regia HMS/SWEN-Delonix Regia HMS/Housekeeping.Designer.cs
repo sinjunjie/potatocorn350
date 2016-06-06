2c3b5aac869bc8c916f9dd4fb78abc88f4e5f50f
riduwan2016 <mdriduwan@hotmail.com>
2016-06-06 21:37:24 +0800

    added crud button

--- SWEN-Delonix Regia HMS/SWEN-Delonix Regia HMS/Housekeeping.Designer.cs
+++ SWEN-Delonix Regia HMS/SWEN-Delonix Regia HMS/Housekeeping.Designer.cs
@@ -28,13 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.btnCreate = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -45,47 +40,23 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tbxFirstName = new System.Windows.Forms.TextBox();
+            this.tbxDOB = new System.Windows.Forms.TextBox();
+            this.tbxLastName = new System.Windows.Forms.TextBox();
+            this.tbxBankAcc = new System.Windows.Forms.TextBox();
+            this.tbxStaffAdd = new System.Windows.Forms.TextBox();
+            this.tbxPhoneNum = new System.Windows.Forms.TextBox();
+            this.tbxAccId = new System.Windows.Forms.TextBox();
+            this.tbxDutyId = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(232, 17);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 41);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Update";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(78, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(75, 80);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(78, 38);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(232, 119);
+            this.button2.Location = new System.Drawing.Point(327, 321);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
@@ -95,114 +66,210 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(78, 122);
+            this.textBox3.Location = new System.Drawing.Point(206, 326);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 5;
             // 
-            // btnCreate
-            // 
-            this.btnCreate.Location = new System.Drawing.Point(232, 189);
-            this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(75, 23);
-            this.btnCreate.TabIndex = 6;
-            this.btnCreate.Text = "Create";
-            this.btnCreate.UseVisualStyleBackColor = true;
-            // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(27, 260);
+            this.textBox4.Location = new System.Drawing.Point(77, 4);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 7;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(133, 258);
+            this.button3.Location = new System.Drawing.Point(183, 4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
+            this.button3.Text = "Retrieve";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(447, 45);
+            this.label2.Location = new System.Drawing.Point(11, 41);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 9;
-            this.label2.Text = "label2";
+            this.label2.Text = "FirstName";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(447, 82);
+            this.label3.Location = new System.Drawing.Point(21, 71);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 10;
-            this.label3.Text = "label3";
+            this.label3.Text = "LastName";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(447, 119);
+            this.label4.Location = new System.Drawing.Point(11, 111);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(68, 13);
             this.label4.TabIndex = 12;
-            this.label4.Text = "label4";
+            this.label4.Text = "Date Of Birth";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(447, 156);
+            this.label5.Location = new System.Drawing.Point(11, 148);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(86, 13);
             this.label5.TabIndex = 11;
-            this.label5.Text = "label5";
+            this.label5.Text = "BankAccountNo";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(447, 189);
+            this.label6.Location = new System.Drawing.Point(12, 184);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(70, 13);
             this.label6.TabIndex = 16;
-            this.label6.Text = "label6";
+            this.label6.Text = "Staff Address";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(447, 220);
+            this.label7.Location = new System.Drawing.Point(9, 212);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(78, 13);
             this.label7.TabIndex = 15;
-            this.label7.Text = "label7";
+            this.label7.Text = "Phone Number";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(447, 258);
+            this.label8.Location = new System.Drawing.Point(30, 250);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.Size = new System.Drawing.Size(41, 13);
             this.label8.TabIndex = 14;
-            this.label8.Text = "label8";
+            this.label8.Text = "Duty Id";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(447, 296);
+            this.label9.Location = new System.Drawing.Point(17, 288);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.Size = new System.Drawing.Size(59, 13);
             this.label9.TabIndex = 13;
-            this.label9.Text = "label9";
+            this.label9.Text = "Account Id";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(15, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "StaffById";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(150, 329);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(50, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "StaffById";
+            // 
+            // tbxFirstName
+            // 
+            this.tbxFirstName.Location = new System.Drawing.Point(77, 34);
+            this.tbxFirstName.Name = "tbxFirstName";
+            this.tbxFirstName.Size = new System.Drawing.Size(149, 20);
+            this.tbxFirstName.TabIndex = 21;
+            // 
+            // tbxDOB
+            // 
+            this.tbxDOB.Location = new System.Drawing.Point(82, 108);
+            this.tbxDOB.Name = "tbxDOB";
+            this.tbxDOB.Size = new System.Drawing.Size(149, 20);
+            this.tbxDOB.TabIndex = 22;
+            // 
+            // tbxLastName
+            // 
+            this.tbxLastName.Location = new System.Drawing.Point(82, 71);
+            this.tbxLastName.Name = "tbxLastName";
+            this.tbxLastName.Size = new System.Drawing.Size(149, 20);
+            this.tbxLastName.TabIndex = 23;
+            // 
+            // tbxBankAcc
+            // 
+            this.tbxBankAcc.Location = new System.Drawing.Point(103, 145);
+            this.tbxBankAcc.Name = "tbxBankAcc";
+            this.tbxBankAcc.Size = new System.Drawing.Size(149, 20);
+            this.tbxBankAcc.TabIndex = 24;
+            // 
+            // tbxStaffAdd
+            // 
+            this.tbxStaffAdd.Location = new System.Drawing.Point(86, 181);
+            this.tbxStaffAdd.Name = "tbxStaffAdd";
+            this.tbxStaffAdd.Size = new System.Drawing.Size(149, 20);
+            this.tbxStaffAdd.TabIndex = 25;
+            // 
+            // tbxPhoneNum
+            // 
+            this.tbxPhoneNum.Location = new System.Drawing.Point(86, 212);
+            this.tbxPhoneNum.Name = "tbxPhoneNum";
+            this.tbxPhoneNum.Size = new System.Drawing.Size(149, 20);
+            this.tbxPhoneNum.TabIndex = 26;
+            // 
+            // tbxAccId
+            // 
+            this.tbxAccId.Location = new System.Drawing.Point(86, 288);
+            this.tbxAccId.Name = "tbxAccId";
+            this.tbxAccId.Size = new System.Drawing.Size(149, 20);
+            this.tbxAccId.TabIndex = 27;
+            // 
+            // tbxDutyId
+            // 
+            this.tbxDutyId.Location = new System.Drawing.Point(86, 250);
+            this.tbxDutyId.Name = "tbxDutyId";
+            this.tbxDutyId.Size = new System.Drawing.Size(149, 20);
+            this.tbxDutyId.TabIndex = 28;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(317, 60);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 30;
+            this.button4.Text = "Create";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Housekeeping
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(689, 358);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxDutyId);
+            this.Controls.Add(this.tbxAccId);
+            this.Controls.Add(this.tbxPhoneNum);
+            this.Controls.Add(this.tbxStaffAdd);
+            this.Controls.Add(this.tbxBankAcc);
+            this.Controls.Add(this.tbxLastName);
+            this.Controls.Add(this.tbxDOB);
+            this.Controls.Add(this.tbxFirstName);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label8);
@@ -213,13 +280,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Housekeeping";
             this.Text = "Housekeeping";
             this.Load += new System.EventHandler(this.Housekeeping_Load);
@@ -230,13 +292,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button btnCreate;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label2;
@@ -247,6 +304,18 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox tbxFirstName;
+        private System.Windows.Forms.TextBox tbxDOB;
+        private System.Windows.Forms.TextBox tbxLastName;
+        private System.Windows.Forms.TextBox tbxBankAcc;
+        private System.Windows.Forms.TextBox tbxStaffAdd;
+        private System.Windows.Forms.TextBox tbxPhoneNum;
+        private System.Windows.Forms.TextBox tbxAccId;
+        private System.Windows.Forms.TextBox tbxDutyId;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
 
 
     }
