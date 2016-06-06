13db2d8192357224a490d82aea1f30f8d458c0d9
nigelywp <1400897c@student.tp.edu.sg>
2016-06-06 19:03:20 +0800

    test

--- SWEN-Delonix Regia HMS/SWEN-Delonix Regia HMS/RoomAvailability.Designer.cs
+++ SWEN-Delonix Regia HMS/SWEN-Delonix Regia HMS/RoomAvailability.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // RoomAvailability
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 354);
+            this.Name = "RoomAvailability";
             this.Text = "RoomAvailability";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
